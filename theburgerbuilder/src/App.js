38f3b615a68838ebd5368e3c45457f024b50921f
Ognen Kirov Todorovski <35491291+Ognen67@users.noreply.github.com>
2021-03-04 23:10:32 +0100

    Adding options to the menu

--- theburgerbuilder/src/App.js
+++ theburgerbuilder/src/App.js
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css';
-import Burger from './burger'
-import {render} from "@testing-library/react";
+import Burger from './components/burger'
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        console.log('[App.js] constructor', props)
+        this.state = {
+            burgers: [
+                { name: 'classic', price: 130 },
+                { name: 'cheese', price: 140 },
+                { name: 'double', price: 180 },
+                { name: 'double extra sauce', price: 195 },
+            ],
+            isOpened : false
+        }
+    }
+
+    toggleMenu = () => {
+        this.setState({
+            isOpened: !this.state.isOpened
+        })
+    }
+
     render() {
-    return (
-<div className="App">
-<header>
-<h1>The Burger Builder</h1>
-</header>
-<Burger num={1}/>
-<Burger num={2}/>
-<Burger num={3}/>
-</div>
-);
-}
+        let burgers = null
+        if (this.state.isOpened) {
+            burgers = (
+                <div className={'burgers'}>
+                    {this.state.burgers.map((burger, index) =>
+                        <Burger name={burger.name} price={burger.price} key={index}/>
+                    )}
+                </div>
+            )
+        }
 
+        console.log('[App.js] render')
+        return (
+            <div className="App">
+                <header className={"App-header"}>
+                    <h1>The Burger Builder</h1>
+                    {burgers}
 
+                    <button onClick={this.toggleMenu}>Show menu</button>
+                </header>
 
+            </div>
+        )
+    }
 }
 
 export default App;
--- theburgerbuilder/src/burger.js
+++
@@ -1,3 +0,0 @@
-const burger = (props) => <p>This is a burger {props.num}</p>
-
-export default burger;
